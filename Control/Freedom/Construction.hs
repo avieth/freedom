b3f40aa525c5e895ad4493ee51d55e5087d6c4f3
Alexander Vieth <alexander.vieth@mail.mcgill.ca>
2015-09-17 15:30:47 -0400

    Move Pure, Rec and friends into Construction.hs

--- Control/Freedom/Construction.hs
+++ Control/Freedom/Construction.hs
@@ -35,6 +35,13 @@ necessary to implement
 
 which is very useful for writing terms of @Fix f s t@.
 
+Since @Fix f@ is defined here, and since we would like variations of @Fix f@ to
+be instances of @Functor@, @Profunctor@, @Applicative@, @Alternative@, @Arrow@,
+@ArrowChoice@, @Monad@ and others, depending upon the summands present in
+@f@, we also define many relevant datatypes here--the datatype which must be
+present in @f@ in order to achieve the relevant instances in @Fix f@. Defining
+them in other modules would yield orphan instances.
+
 -}
 
 {-# LANGUAGE AutoDeriveTypeable #-}
@@ -56,15 +63,31 @@ which is very useful for writing terms of @Fix f s t@.
 
 module Control.Freedom.Construction (
 
-      Fix(..)
+      type (+)
+    , Fix(..)
     , inj
     , Inject2
+    , inject2
+
+    , Rec(..)
+    , Close(..)
+    , Pure(..)
+    , Dimap(..)
+    , Sequence(..)
+    , First(..)
+    , Apply(..)
+    , Alternative(..)
+    , Join(..)
 
     ) where
 
 import Prelude hiding ((.), id)
 import Control.Category
+import Control.Applicative hiding (Alternative)
+import qualified Control.Applicative as Applicative
+import Control.Arrow
 import Data.Proxy
+import Data.Profunctor
 
 data FSum (f :: (* -> * -> *) -> * -> * -> *) (g :: (* -> * -> *) -> * -> * -> *) (h :: * -> * -> *) (s :: *) (t :: *) where
     FSumL :: (f h) s t -> FSum f g h s t
@@ -127,3 +150,115 @@ newtype Fix f s t = Fix {
 
 inj :: Inject2 f g => f (Fix g) a b -> Fix g a b
 inj = Fix . inject2
+
+-- Would be nice to have
+--
+--   @inj' :: Fix f s t -> Fix g s t@
+--
+-- whenever @f@ is a sub-sum of @g@ (every @f@ summand is in @g@).
+--
+-- Is this possible? We runFix to get an @f (Fix f) s t@ and we must somehow
+-- recursively @inject2@ each @f@-term into @g@ to recover @g (Fix g)@.
+-- The issue is that @inject2@ demands the parameters @hs@ be the same for
+-- both terms. What we need is
+--
+--   @ftrans :: (forall s t . h s t -> h' s t) -> f h s t -> f h' s t@
+--
+-- This is TODO; not *that* imporant, but desirable.
+--
+-- We'll want to use typeclasses to resolve this automatically... 
+
+-- | When a @Rec@ is part of a @Fix@ed sum @f@, it represents any term of
+--   @Fix f@.
+data Rec (h :: * -> * -> *) (s :: *) (t :: *) where
+    Rec :: h s t -> Rec h s t
+
+-- | Close a datatype, so that it ignores the @h@ parameter. If you use a
+--   @Close f@ in a sum @g@, then @Fix g@ does not include @f (Fix g)@.
+data Close (f :: (* -> * -> *) -> * -> * -> *) (h :: * -> * -> *) (s :: *) (t :: *) where
+    Close :: Fix f s t -> Close f h s t
+
+-- | When a @Pure@ is part of a @Fix@ed sum @f@, it represents any Haskell
+--   function.
+data Pure (h :: * -> * -> *) (s :: *) (t :: *) where
+    Pure :: ((->) s t) -> Pure h s t
+
+data Dimap (f :: (* -> * -> *) -> * -> * -> *) (h :: * -> * -> *) (s :: *) (t :: *) where
+    Dimap :: (s -> s') -> (t' -> t) -> f h s' t' -> Dimap f h s t
+
+data Sequence (left :: (* -> * -> *) -> * -> * -> *) (right :: (* -> * -> *) -> * -> * -> *) (h :: * -> * -> *) (s :: *) (t :: *) where
+    Sequence :: left h t u -> right h s t -> Sequence left right h s u
+
+data First (f :: (* -> * -> *) -> * -> * -> *) (h :: * -> * -> *) (s :: *) (t :: *) where
+    First :: f h s t -> First f h (s, c) (t, c)
+
+data Apply (f :: (* -> * -> *) -> * -> * -> *) (g :: (* -> * -> *) -> * -> * -> *) (h :: * -> * -> *) (s :: *) (t :: *) where
+    Apply :: f h s (t -> u) -> g h s t -> Apply f g h s u
+
+data Empty (h :: * -> * -> *) (s :: *) (t :: *) where
+    Empty :: Empty h s t
+
+data Alternative (f :: (* -> * -> *) -> * -> * -> *) (g :: (* -> * -> *) -> * -> * -> *) (h :: * -> * -> *) (s :: *) (t :: *) where
+    Alternative :: f h s t -> g h s t -> Alternative f g h s t
+
+data Join (f :: (* -> * -> *) -> * -> * -> *) (g :: (* -> * -> *) -> * -> * -> *) (h :: * -> * -> *) (s :: *) (t :: *) where
+    Join :: f h s (g h s t) -> Join f g h s t
+
+instance
+    ( Inject2 (Dimap Rec) f
+    ) => Profunctor (Fix f)
+  where
+    dimap l r term = inj (Dimap l r (Rec term))
+
+instance
+    ( Inject2 (Dimap Rec) f
+    ) => Functor (Fix f s)
+  where
+    fmap = rmap
+
+instance
+    ( Inject2 Pure f
+    , Inject2 (Sequence Rec Rec) f
+    ) => Category (Fix f)
+  where
+    id = inj (Pure id)
+    (.) left right = inj (Sequence (Rec left) (Rec right))
+
+instance
+    ( Inject2 Pure f
+    , Inject2 (Dimap Rec) f
+    , Inject2 (Apply Rec Rec) f
+    ) => Applicative (Fix f s)
+  where
+    pure = inj . Pure . const
+    (<*>) left right = inj (Apply (Rec left) (Rec right))
+
+instance
+    ( Inject2 Pure f
+    , Inject2 (Dimap Rec) f
+    , Inject2 (Apply Rec Rec) f
+    , Inject2 Empty f
+    , Inject2 (Alternative Rec Rec) f
+    ) => Applicative.Alternative (Fix f s)
+  where
+    empty = inj Empty
+    (<|>) left right = inj (Alternative (Rec left) (Rec right))
+
+instance
+    ( Inject2 Pure f
+    , Inject2 (Sequence Rec Rec) f
+    , Inject2 (First Rec) f
+    ) => Arrow (Fix f)
+  where
+    arr = inj . Pure
+    first = inj . First . Rec
+
+instance
+    ( Inject2 Pure f
+    , Inject2 (Dimap Rec) f
+    , Inject2 (Apply Rec Rec) f
+    , Inject2 (Join Rec Rec) f
+    ) => Monad (Fix f s)
+  where
+    return = pure
+    (>>=) x k = inj (Join (Rec (fmap (Rec . k) x)))
