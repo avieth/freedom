98fac4af80bf43094aa6163cf735708dd88d4965
Alexander Vieth <alexander.vieth@mail.mcgill.ca>
2015-09-21 16:13:40 -0400

    Update interpretation for changes in cosntruction

--- Control/Freedom/Interpretation.hs
+++ Control/Freedom/Interpretation.hs
@@ -25,6 +25,7 @@ by providing injections of each summand into a common target.
 {-# LANGUAGE Arrows #-}
 {-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Control.Freedom.Interpretation (
 
@@ -37,14 +38,17 @@ module Control.Freedom.Interpretation (
     , runInterpreter
     , Interprets
     , interpret
-    , runFix
-    , profunctorInterpreter
-    , categoryInterpreter
-    , applicativeInterpreter
-    , alternativeInterpreter
-    , arrowInterpreter
-    , arrowChoiceInterpreter
-    , monadInterpreter
+    , interpretFix
+
+    , recInterpreter
+    , closeInterpreter
+    , pureInterpreter
+    , noneInterpreter
+    , dimapInterpreter
+    , dotInterpreter
+    , sequenceInterpreter
+    , applyInterpreter
+    , joinInterpreter
 
     ) where
 
@@ -88,27 +92,59 @@ instance
     ) => Interprets (i % j) h (f1 + f2) g
   where
     interpret (left, right) = \recurse sumTerm -> case sumTerm of
-        F2SumLeft term -> interpret left recurse term
-        F2SumRight term -> interpret right recurse term
+        FSumL term -> interpret left recurse term
+        FSumR term -> interpret right recurse term
 
-runFix :: Interprets i (Fix f) f g => i -> Fix f s t -> g s t
-runFix i fixTerm = case fixTerm of
-    Fix term -> interpret i (runFix i) term
+interpretFix :: Interprets i (Fix f) f g => i -> Fix f s t -> g s t
+interpretFix i fixTerm = case fixTerm of
+    Fix term -> interpret i (interpretFix i) term
 
-profunctorInterpreter :: Profunctor g => Interpreter h FProfunctor g
-profunctorInterpreter = Interpreter $ \recurse term -> case term of
-    FProfunctorDimap l r term -> dimap l r (recurse term)
+recInterpreter :: Interpreter h Rec g
+recInterpreter = Interpreter $ \recurse term -> case term of
+    Rec subterm -> recurse subterm
 
-categoryInterpreter :: Category g => Interpreter h FCategory g
-categoryInterpreter = Interpreter $ \recurse term -> case term of
-    FCategoryId -> id
-    FCategoryComp left right -> recurse left . recurse right
+closeInterpreter :: Interpreter (Fix f) (Close f) g
+closeInterpreter = Interpreter $ \recurse term -> case term of
+    Close fixTerm -> recurse fixTerm
 
-applicativeInterpreter :: ApArrow g => Interpreter h FApplicative g
-applicativeInterpreter = Interpreter $ \recurse term -> case term of
-    FApplicativePure x -> aaPure x
-    FApplicativeAp f x -> recurse f `aaApply` recurse x
+pureInterpreter :: Interpreter h Pure (->)
+pureInterpreter = Interpreter $ \recurse term -> case term of
+    Pure f -> f
 
+noneInterpreter :: g () () -> Interpreter h None g
+noneInterpreter value = Interpreter $ \recurse term -> case term of
+    None -> value
+
+dimapInterpreter :: Profunctor g => Interpreter h f g -> Interpreter h (Dimap f) g
+dimapInterpreter finterpreter = Interpreter $ \recurse term -> case term of
+    Dimap l r term -> dimap l r ((runInterpreter finterpreter) recurse term)
+
+dotInterpreter
+    :: (Category g)
+    => Interpreter h left g
+    -> Interpreter h right g
+    -> Interpreter h (Dot left right) g
+dotInterpreter leftInterpreter rightInterpreter = Interpreter $ \recurse term -> case term of
+    Dot left right -> (runInterpreter leftInterpreter recurse left) . (runInterpreter rightInterpreter recurse right)
+
+sequenceInterpreter
+    :: ( forall s t . g s () -> g s t -> g s t )
+    -> Interpreter h left g
+    -> Interpreter h right g
+    -> Interpreter h (Sequence left right) g
+sequenceInterpreter next leftInterpreter rightInterpreter = Interpreter $ \recurse term -> case term of
+    Sequence left right -> (runInterpreter leftInterpreter recurse left) `next` (runInterpreter rightInterpreter recurse right)
+
+applyInterpreter
+    :: ( )
+    => (forall s t u . g s (t -> u) -> g s t -> g s u )
+    -> Interpreter h f1 g
+    -> Interpreter h f2 g
+    -> Interpreter h (Apply f1 f2) g
+applyInterpreter apply f1interpreter f2interpreter = Interpreter $ \recurse term -> case term of
+    Apply f1 f2 -> (runInterpreter f1interpreter recurse f1) `apply` (runInterpreter f2interpreter recurse f2)
+
+{-
 alternativeInterpreter :: AltArrow g => Interpreter h FAlternative g
 alternativeInterpreter = Interpreter $ \recurse term -> case term of
     FAlternativeEmpty -> altEmpty
@@ -122,120 +158,14 @@ arrowInterpreter = Interpreter $ \recurse term -> case term of
 arrowChoiceInterpreter :: ArrowChoice g => Interpreter h FArrowChoice g
 arrowChoiceInterpreter = Interpreter $ \recurse term -> case term of
     FLeft term -> left (recurse term)
-
-monadInterpreter :: KleisliArrow g => Interpreter h FMonad g
-monadInterpreter = Interpreter $ \recurse term -> case term of
-    FMonadReturn x -> kaReturn x
-    FMonadBind x k -> recurse x `kaBind` (recurse . k)
-
-{-
-data Ex1 f s t where
-    Add :: Ex1 f (Int, Int) Int
-
-data Ex2 u f s t where
-    Log :: Ex2 u f u ()
-
-type Ex = Fix (FArrow + FCategory + Ex1 + Ex2 String)
-
-exTerm :: Ex (Int, Int) Int
-exTerm = proc (x, y) -> do
-    sum <- inj Add -< (x, y)
-    () <- inj Log -< show sum
-    returnA -< sum
-
-ex1Interpreter :: Interpreter h Ex1 (Kleisli IO)
-ex1Interpreter = Interpreter $ \_ term -> case term of
-    Add -> Kleisli $ \(x, y) -> return (x + y)
-
-ex2Interpreter :: Show u => Interpreter h (Ex2 u) (Kleisli IO)
-ex2Interpreter = Interpreter $ \_ term -> case term of
-    Log -> Kleisli $ \s -> print s
-
-{-
-exInterpreter
-    :: Interpreter Ex FArrow (Kleisli IO)
-    %  Interpreter Ex FCategory (Kleisli IO)
-    %  Interpreter Ex Ex1 (Kleisli IO)
-    %  Interpreter Ex (Ex2 String) (Kleisli IO)
--}
-exInterpreter :: (FArrow + FCategory + Ex1 + Ex2 String) ~> (Kleisli IO)
-exInterpreter =
-      arrowInterpreter
-    % categoryInterpreter
-    % ex1Interpreter
-    % ex2Interpreter
 -}
 
-{-
-data Interpreter (f :: * -> * -> *) (g :: * -> * -> *)  where
-    Interpreter :: (forall s t . f s t -> g s t) -> Interpreter f g
-
-runInterpreter :: Interpreter f g -> (forall s t . f s t -> g s t)
-runInterpreter term = case term of
-    Interpreter f -> f
-
-class Interprets i f g where
-    interpret :: i -> (forall s t . f s t -> g s t)
-
-instance Interprets (Interpreter f g) f g where
-    interpret = runInterpreter
-
-instance
-    ( Interprets i (f1 h) g
-    , Interprets j (f2 h) g
-    ) => Interprets (i % j) ((f1 + f2) h) g
-  where
-    interpret (left, right) = \sumTerm -> case sumTerm of
-        F2SumLeft term -> interpret left term
-        F2SumRight term -> interpret right term
-
-instance
-    ( Interprets i (f (Fix f)) g
-    ) => Interprets i (Fix f) g
-  where
-    interpret i = \fixTerm -> case fixTerm of
-        Fix term -> interpret i term
-
-categoryInterpreter :: Category g => (forall s t . h s t -> g s t) -> Interpreter (FCategory h) g
-categoryInterpreter recurse = Interpreter $ \term -> case term of
-    FCategoryId -> id
-    FCategoryComp left right -> recurse left . recurse right
-
-arrowInterpreter :: Arrow g => (forall s t . h s t -> g s t) -> Interpreter (FArrow h) g
-arrowInterpreter recurse = Interpreter $ \term -> case term of
-    FArrow f -> arr f
-    FFirst term -> first (recurse term)
-
-data Ex1 f s t where
-    Add :: Ex1 f (Int, Int) Int
-
-data Ex2 u f s t where
-    Log :: Ex2 u f u ()
-
-type Ex = Fix (FArrow + FCategory + Ex1 + Ex2 String)
-
-exTerm :: Ex (Int, Int) Int
-exTerm = proc (x, y) -> do
-    sum <- inj Add -< (x, y)
-    () <- inj Log -< show sum
-    returnA -< sum
-
-ex1Interpreter :: Interpreter (Ex1 h) (Kleisli IO)
-ex1Interpreter = Interpreter $ \term -> case term of
-    Add -> Kleisli $ \(x, y) -> return (x + y)
-
-ex2Interpreter :: Show u => Interpreter (Ex2 u h) (Kleisli IO)
-ex2Interpreter = Interpreter $ \term -> case term of
-    Log -> Kleisli $ \s -> print s
-
-exInterpreter
-    :: Interpreter (FArrow Ex) (Kleisli IO)
-    %  Interpreter (FCategory Ex) (Kleisli IO)
-    %  Interpreter (Ex1 Ex) (Kleisli IO)
-    %  Interpreter (Ex2 String Ex) (Kleisli IO)
-exInterpreter =
-      arrowInterpreter (interpret exInterpreter)
-    % categoryInterpreter (interpret exInterpreter)
-    % ex1Interpreter
-    % ex2Interpreter
--}
+joinInterpreter 
+    :: ( )
+    => (forall s t u . (t -> u) -> g s t -> g s u)
+    -> (forall s t u . (g s (g s u)) -> g s u)
+    -> Interpreter h f1 g
+    -> Interpreter h f2 g
+    -> Interpreter h (Join f1 f2) g
+joinInterpreter fmap join f1interpreter f2interpreter = Interpreter $ \recurse term -> case term of
+    Join term -> join (fmap (runInterpreter f2interpreter recurse) (runInterpreter f1interpreter recurse term))
